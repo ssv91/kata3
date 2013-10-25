ccd3fc8c33b5a836a2e80d221abc41b6058fda21
usuario <usuario@10.22.146.207>
2013-10-25 10:59:59 +0100

    Third commit

---
+++ src/kata3/persistence/MailReader.java
@@ -0,0 +1,9 @@
+package kata3.persistence;
+
+public class MailReader {
+
+    public String[] readDomains(String filename) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
